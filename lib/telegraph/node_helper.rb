21089e9802fbc07ccc5e02f054fc905319a671a9
Aleksey Kiselev <Aleksey@AlekseyMac.local>
2017-09-17 13:40:51 +0300

    Node Helper done

--- lib/telegraph/node_helper.rb
+++ lib/telegraph/node_helper.rb
@@ -5,17 +5,23 @@ module NodeHelper
         if child.name == 'text'
           child.text
         else
-          children2 = child.children.map do |child2|
-            if child2.name == 'text'
-              child2.text
-            else
-              child2.to_node
-            end
-          end
-          {"tag": child.name, "children": children2}
+          process_children(child)
         end
       end
       {"tag": self.name, "children": children}
+    end
+
+    private
+
+    def process_children(child)
+      children = child.children.map do |child2|
+        if child2.name == 'text'
+          child2.text
+        else
+          child2.to_node
+        end
+      end
+      { "tag": child.name, "children": children }
     end
   end
 end
